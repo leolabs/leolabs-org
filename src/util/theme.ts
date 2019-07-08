cd997dec489774056d18845625bee9f5be4d857c
Leo Bernard <admin@leolabs.org>
2019-07-09 00:43:40 +0200

    :lipstick: Improve font sizes

--- src/util/theme.ts
+++ src/util/theme.ts
@@ -22,12 +22,12 @@ export type Mode = keyof typeof themes;
 export const dimensions = {
   maxWidth: '80rem',
   fontSizes: {
-    h1: '3.052rem',
-    h2: '2.441rem',
-    h3: '1.953rem',
-    h4: '1.563rem',
-    h5: '1.25rem',
-    p: '1.125rem',
+    h1: '2.5em',
+    h2: '2.2em',
+    h3: '1.953em',
+    h4: '1.563em',
+    h5: '1.25em',
+    p: '1em',
   },
   borderRadius: {
     normal: '0.5rem',
