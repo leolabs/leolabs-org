63365486ac13078154a634b2a98cf649f82ac3ee
Leo Bernard <leo@leolabs.org>
2023-11-12 13:03:11 +0100

    :wrench: Always add trailing slashes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   siteMetadata: {
     siteUrl: 'https://leolabs.org',
   },
+  trailingSlash: "always",
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-styled-components`,
